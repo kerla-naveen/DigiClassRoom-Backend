f583f933e0399d29853d84baf7627d08d4a38657
Naveen Kerla <kerlanaveen19@gmail.com>
2026-01-24 23:05:49 +0530

    CORS updated

--- src/app.js
+++ src/app.js
@@ -4,22 +4,10 @@ import { errorHandler } from './middlewares/error.middleware.js'
 
 const app=express()
 
-const DEFAULT_ORIGINS = [
+const ALLOWED_ORIGINS = [
+  'https://digiclassroom-frontend.netlify.app',
   'http://localhost:8080',
-  'http://127.0.0.1:8080',
-  'http://localhost:8081',
-  'http://127.0.0.1:8081',
-  'http://localhost:8084',
-  'http://127.0.0.1:8084',
-  'http://10.44.155.11:8080'
 ];
-
-const ENV_ORIGINS = (process.env.CORS_ORIGIN || process.env.cors_ORIGIN || '')
-  .split(',')
-  .map((s) => s.trim())
-  .filter(Boolean);
-
-const ALLOWED_ORIGINS = ENV_ORIGINS.length ? ENV_ORIGINS : DEFAULT_ORIGINS;
 
 app.use(cors({
   origin(origin, callback) {
