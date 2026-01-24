1d59fe77171db7376955bfba46615bdec22f6a22
Naveen Kerla <kerlanaveen19@gmail.com>
2026-01-24 23:20:08 +0530

    fixed CORS issue

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ app.use(cors({
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  maxAge: 86400,
 }));
 
 
